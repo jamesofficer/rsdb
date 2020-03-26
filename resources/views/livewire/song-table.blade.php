9df3e33a4d0291042cd64006c20849b552aa862a
James Officer <james.d.officer@gmail.com>
2020-03-26 22:13:54 +1100

    Click on a row to view a song.

--- resources/views/livewire/song-table.blade.php
+++ resources/views/livewire/song-table.blade.php
@@ -2,7 +2,7 @@
     {{-- Filter Search Box --}}
     <div class="my-8">
         <h2 class="mt-8 font-bold text-blue-700 text-2xl">Filter Songs</h2>
-        <p class="mb-4">You can search by artist name, album title, pack name etc.</p>
+        <p class="mb-4">You can search by artist name, album title, pack name etc. Click on a row to see more information about a song.</p>
         <input wire:model="query" type="text" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-2xl text-gray-700 leading-tight focus:outline-none focus:bg-gray-100 focus:border-blue-600">
     </div>
 
@@ -33,9 +33,9 @@
 
                     <tbody class="bg-gray-100">
                         @foreach ($songs as $song)
-                        <tr>
+                        <tr wire:click="selectSong('{{ $song->id }}')">
                             <td class="songsmith-td font-bold">
-                                <span wire:click="selectSong('{{ $song->id }}')" class="cursor-pointer hover:underline">
+                                <span class="cursor-pointer">
                                     {{ $song->title }}
                                 </span>
                             </td>
