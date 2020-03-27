a28f4fcd855ef56673c70ab03bfae90be1eca089
James Officer <james.d.officer@gmail.com>
2020-03-27 16:49:59 +1100

    Add purgecss.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,12 @@
 const mix = require("laravel-mix");
 const tailwindcss = require("tailwindcss");
+require("laravel-mix-purgecss");
 
-mix.sass("resources/sass/app.scss", "public/css").options({
-    processCssUrls: false,
-    postCss: [tailwindcss("./tailwind.config.js")]
-});
+mix.sass("resources/sass/app.scss", "public/css")
+    .options({
+        processCssUrls: false,
+        postCss: [tailwindcss("./tailwind.config.js")]
+    })
+    .purgeCss({
+        enabled: true
+    });
